9ffb69a5f58e076cf10ea5b594db02cde06f1802
Dito <ddito@deloitte.com>
2018-11-15 15:32:54 -0700

    fixed port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require("path");
+const PORT = process.env.PORT || 3000;
 const app = express();
 const status = ["down", "up", "degraded"];
 
@@ -22,6 +23,6 @@ app.get('/data', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log("Listening");
+app.listen(PORT, ()=> {
+  console.log("Server is listening on port" + PORT);
 });
